28ea72a0a92f86f93363f2e23d589a100f1eed65
levlam <levlam@telegram.org>
2026-07-27 11:07:43 +0300

    Add td_api::welcomeMessage.

--- td/telegram/WelcomeMessageManager.cpp
+++ td/telegram/WelcomeMessageManager.cpp
@@ -6,13 +6,30 @@
 //
 #include "td/telegram/WelcomeMessageManager.h"
 
+#include "td/telegram/DialogId.h"
+#include "td/telegram/MessageContent.h"
+#include "td/telegram/MessageId.h"
+#include "td/telegram/Td.h"
+#include "td/telegram/UserId.h"
+
 namespace td {
+
+WelcomeMessageManager::WelcomeMessage::~WelcomeMessage() = default;
 
 WelcomeMessageManager::WelcomeMessageManager(Td *td, ActorShared<> parent) : td_(td), parent_(std::move(parent)) {
 }
 
 void WelcomeMessageManager::tear_down() {
   parent_.reset();
+}
+
+td_api::object_ptr<td_api::welcomeMessage> WelcomeMessageManager::get_welcome_message_object(
+    const WelcomeMessage *m) const {
+  CHECK(m != nullptr);
+  auto content = get_message_content_object(m->content_.get(), td_, DialogId(), MessageId(), DialogId(), false, false,
+                                            false, DialogId(), 0, 0, false, true, -1, m->invert_media_,
+                                            m->disable_web_page_preview_, "get_welcome_message_object");
+  return td_api::make_object<td_api::welcomeMessage>(m->ephemeral_message_id_.get(), std::move(content));
 }
 
 }  // namespace td
--- td/telegram/WelcomeMessageManager.h
+++ td/telegram/WelcomeMessageManager.h
@@ -6,12 +6,17 @@
 //
 #pragma once
 
+#include "td/telegram/EphemeralMessageId.h"
+#include "td/telegram/MessageContentUploadId.h"
+#include "td/telegram/td_api.h"
+
 #include "td/actor/actor.h"
 
 #include "td/utils/common.h"
 
 namespace td {
 
+class MessageContent;
 class Td;
 
 class WelcomeMessageManager final : public Actor {
@@ -19,7 +24,25 @@ class WelcomeMessageManager final : public Actor {
   WelcomeMessageManager(Td *td, ActorShared<> parent);
 
  private:
+  struct WelcomeMessage {
+    EphemeralMessageId ephemeral_message_id_;
+    bool invert_media_ = false;
+    bool disable_web_page_preview_ = false;
+    unique_ptr<MessageContent> content_;
+
+    MessageContentUploadId upload_id_;  // for add_welcome_message/edit_welcome_message
+
+    WelcomeMessage() = default;
+    WelcomeMessage(const WelcomeMessage &) = delete;
+    WelcomeMessage &operator=(const WelcomeMessage &) = delete;
+    WelcomeMessage(WelcomeMessage &&) = delete;
+    WelcomeMessage &operator=(WelcomeMessage &&) = delete;
+    ~WelcomeMessage();
+  };
+
   void tear_down() final;
+
+  td_api::object_ptr<td_api::welcomeMessage> get_welcome_message_object(const WelcomeMessage *m) const;
 
   Td *td_;
   ActorShared<> parent_;
